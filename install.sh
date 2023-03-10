fd7856ba130d8e79bc09a4ec2cca091b87260eba
Johnny O'Donnell <odjohnny@amazon.com>
2023-03-10 13:49:37 -0800

    Use recursive copy on bin
    Create cron job in /etc/cron.d

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ git clone --depth=1 git@github.com:johnnyodonnell/sync-notes.git
 # Add notes-sync bin to user home folder
 mkdir ~/.sync-notes
 rm -rf ~/.sync-notes/bin
-cp /tmp/sync-notes/bin ~/.sync-notes/bin
+cp -r /tmp/sync-notes/bin ~/.sync-notes/bin
 
 # Create cron job for syncing notes
 CRON_STRING="*/5 * * * *    $USER    /home/$USER/.sync-notes/bin/sync-notes.sh"
-sudo touch sync-notes
-sudo echo $CRON_STRING > sync-notes
+sudo touch /etc/cron.d/sync-notes
+sudo echo $CRON_STRING > /etc/cron.d/sync-notes
 
 # Clean-up
 rm -rf /tmp/sync-notes
