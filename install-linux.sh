78618e981aac86bc576c1e1b39a4cf52694ee093
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2025-05-01 13:46:00 -0700

    Update Linux install

--- install-linux.sh
+++ install-linux.sh
@@ -5,17 +5,24 @@ cd /tmp
 rm -rf /tmp/sync-notes
 git clone --depth=1 git@github.com:johnnyodonnell/sync-notes.git
 
+# Remove old cron file
+CRON_FILENAME=sync-notes-$USER
+sudo rm /etc/cron.d/$CRON_FILENAME
+
 # Add notes-sync bin to user home folder
 mkdir -p ~/.sync-notes
 rm -rf ~/.sync-notes/bin
 cp -r /tmp/sync-notes/bin ~/.sync-notes/bin
 
 # Create cron job for syncing notes
-CRON_FILENAME=sync-notes-$USER
-CRON_FILE="* * * * * $USER ~/.sync-notes/bin/sync-notes.sh"
+read -r -d '' CRON_FILE << EOM
+* * * * * $USER ~/.sync-notes/bin/sync-notes.sh
+EOM
 
 sudo touch /etc/cron.d/$CRON_FILENAME
-echo $CRON_FILE | sudo tee /etc/cron.d/$CRON_FILENAME
+
+# Need quotes to prevent globbing
+echo "$CRON_FILE" | sudo tee /etc/cron.d/$CRON_FILENAME
 
 # Clean-up
 rm -rf /tmp/sync-notes
